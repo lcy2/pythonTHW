9d8735d88fb402572c99ab1e160b0335516afe09
Junxian <Junxian>
2016-09-06 22:02:40 +0100

    Added all the exercise files

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %d old, %s tall and %s heavy." % (
+    int(age), height, weight)
---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, first, second, third = argv
+
+
+moreinput = raw_input("one more input\n")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "One more input is:", moreinput
